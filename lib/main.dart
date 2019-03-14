71396f1079aca67d3ef81b07a33ea25d5fccd76c
AndySong <andylauszp@gmail.com>
2019-03-14 15:29:24 +1300

    model mork some refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
+import './src/app_screens/location_detail.dart';
+import './src/mocks/mock_location.dart';
+import './src/models/location.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final Location _mockLocation = MockLocation.fetchAny();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Andy'),
-        ),
-      ),
-    );
+    return MaterialApp(home: LocationDetail(_mockLocation));
   }
 }
---
+++ lib/src/app_screens/location_detail.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../models/location.dart';
+
+class LocationDetail extends StatelessWidget {
+  final Location _location;
+
+  LocationDetail(this._location);
+
+  Widget _box(String text, {Color color = Colors.red}) {
+    return Container(
+      decoration: BoxDecoration(color: color),
+      child: Text(text),
+    );
+  }
+
+  List<Widget> _renderChildren(Location location) {
+    var result = <Widget>[];
+    location.facts.forEach((fact) {
+      result.add(_title(fact.title));
+      result.add(_text(fact.text));
+    });
+
+    return result;
+  }
+
+  Widget _title(String text) => Text(text);
+  Widget _text(String text) => Text(text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Andy'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _renderChildren(_location),
+      ),
+    );
+  }
+}
