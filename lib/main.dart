9a062f55a5b2c1bc3358a65da983f27e4ed96248
AndySong <andylauszp@gmail.com>
2019-03-21 11:01:04 +1300

    http setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/src/apps/json/json.dart';
+import 'package:flutter_ui/src/apps/http_app/http_app.dart';
 
-void main() => runApp(JsonApp());
+void main() => runApp(HttpApp());
---
+++ lib/src/apps/http_app/http_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/src/apps/http_app/screens/home.dart';
+
+class HttpApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
