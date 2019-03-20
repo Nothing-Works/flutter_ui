993aa19400db4415f7a2b5c7e4e8b8c11b52b307
AndySong <andylauszp@gmail.com>
2019-03-21 11:04:25 +1300

    wip

--- lib/src/apps/http_app/screens/home.dart
+++ lib/src/apps/http_app/screens/home.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/src/apps/http_app/screens/body.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Http App'),
-      ),
-    );
+    return Scaffold(appBar: AppBar(title: Text('Http App')), body: Body());
   }
 }
