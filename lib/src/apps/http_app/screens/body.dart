25ce8127bb1d4fcd47a94ecd118ebca9e913e5a8
AndySong <andylauszp@gmail.com>
2019-03-21 11:14:06 +1300

    wip

--- lib/src/apps/http_app/screens/body.dart
+++ lib/src/apps/http_app/screens/body.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/src/apps/http_app/models/post.dart';
+import 'package:http/http.dart' as http;
 
 class Body extends StatefulWidget {
   @override
@@ -6,10 +10,38 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Post> posts = <Post>[];
+
+  Future loadPosts() async {
+    var response = await http.get('https://jsonplaceholder.typicode.com/posts');
+
+    List content = jsonDecode(response.body);
+
+    var list = content.map((post) => Post.fromJson(post)).toList();
+
+    setState(() {
+      posts = list;
+    });
+  }
+
+  @override
+  void initState() {
+    loadPosts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('center'),
+      child: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (BuildContext context, int index) {
+            var post = posts[index];
+            return ListTile(
+              title: Text(post.title),
+              subtitle: Text(post.body),
+            );
+          }),
     );
   }
 }
