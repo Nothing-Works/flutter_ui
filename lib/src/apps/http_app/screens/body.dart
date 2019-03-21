05441a66176eb60b0ba0f61edcb8bb7f2cd34832
AndySong <andylauszp@gmail.com>
2019-03-21 13:51:23 +1300

    http basic done

--- lib/src/apps/http_app/screens/body.dart
+++ lib/src/apps/http_app/screens/body.dart
@@ -8,27 +8,43 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  Future<List<Post>> _posts;
+
+  @override
+  void initState() {
+    super.initState();
+    _posts = PostRepository.fetchAll();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: FutureBuilder<List<Post>>(
-      future: PostRepository.fetchAll(),
-      builder: (BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                var post = snapshot.data[index];
-                return ListTile(
-                  title: Text(post.title),
-                  subtitle: Text(post.body),
-                );
-              });
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
-        return CircularProgressIndicator();
-      },
-    ));
+      child: _futureBuilder(),
+    );
+  }
+
+  Widget _futureBuilder() {
+    return FutureBuilder<List<Post>>(
+      future: _posts,
+      builder: _futureBuilderItem,
+    );
+  }
+
+  Widget _futureBuilderItem(
+      BuildContext context, AsyncSnapshot<List<Post>> snapshot) {
+    if (snapshot.hasData) {
+      return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (BuildContext context, int index) {
+            var post = snapshot.data[index];
+            return ListTile(
+              title: Text(post.title),
+              subtitle: Text(post.body),
+            );
+          });
+    } else if (snapshot.hasError) {
+      return Text("${snapshot.error}");
+    }
+    return CircularProgressIndicator();
   }
 }
