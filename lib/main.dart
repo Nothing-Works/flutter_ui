70c590b3baf5982611e113b30c296f401a3543f3
AndySong <andylauszp@gmail.com>
2019-03-25 10:51:56 +1300

    bloc setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/src/apps/bloc_vanilla/bloc_vanilla_app.dart';
+import 'package:flutter_ui/src/apps/bloc/bloc_app.dart';
 
-void main() => runApp(BlocVanilla());
+void main() => runApp(BlocApp());
---
+++ lib/src/apps/bloc/bloc_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/src/apps/bloc/screens/home.dart';
+
+class BlocApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocHome(),
+    );
+  }
+}
