f69d4267c2e20d0f9851f52ae7867a95dba8b0a7
AndySong <andylauszp@gmail.com>
2019-03-27 13:09:48 +1300

    setup devtool

--- lib/src/apps/redux/redux_app.dart
+++ lib/src/apps/redux/redux_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_dev_tools/redux_dev_tools.dart';
 
 import './models/app_state.dart';
 import './reducers/app_reducer.dart';
@@ -8,8 +9,10 @@ import './ui/redux_home.dart';
 class ReduxApp extends StatelessWidget {
   final store =
       Store<AppState>(appReducer, initialState: AppState.initialState());
+  final storeDev = DevToolsStore<AppState>(appReducer,
+      initialState: AppState.initialState());
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ReduxHome(store: store));
+    return MaterialApp(home: ReduxHome(store: storeDev));
   }
 }
--- lib/src/apps/redux/ui/redux_home.dart
+++ lib/src/apps/redux/ui/redux_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_redux_dev_tools/flutter_redux_dev_tools.dart';
 import 'package:redux/redux.dart';
 
 import '../actions/counter_actions.dart';
@@ -14,22 +15,22 @@ class ReduxHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreProvider<AppState>(
         child: Scaffold(
-          appBar: AppBar(title: Text('Redux App')),
-          body: StoreConnector<AppState, int>(
-              builder: (BuildContext context, int counter) {
-                return Center(child: Text('$counter'));
-              },
-              converter: (store) => store.state.counter),
-          floatingActionButton: StoreConnector<AppState, VoidCallback>(
-              builder: (BuildContext context, VoidCallback callback) {
-            return FloatingActionButton(
-              onPressed: callback,
-              child: Icon(Icons.add),
-            );
-          }, converter: (store) {
-            return () => store.dispatch(Increment());
-          }),
-        ),
+            appBar: AppBar(title: Text('Redux App')),
+            body: StoreConnector<AppState, int>(
+                builder: (BuildContext context, int counter) {
+                  return Center(child: Text('$counter'));
+                },
+                converter: (store) => store.state.counter),
+            floatingActionButton: StoreConnector<AppState, VoidCallback>(
+                builder: (BuildContext context, VoidCallback callback) {
+              return FloatingActionButton(
+                onPressed: callback,
+                child: Icon(Icons.add),
+              );
+            }, converter: (store) {
+              return () => store.dispatch(Increment());
+            }),
+            endDrawer: ReduxDevTools<AppState>(store)),
         store: store);
   }
 }
